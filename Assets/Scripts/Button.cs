7bfde1e85ed37ab127640b9a40f564e0f267d168
Alexander Shevtsov <114658340+Incarnadine-WP@users.noreply.github.com>
2024-10-09 16:43:49 +0300

    Start Button anim
    update code Button.cs to start anim fade-in fade-out
    change folder name Новая папка to Sprites
    update path naming for assetsGroup

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,37 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Button : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler, IPointerEnterHandler {
-    bool on;
-    Animator animator;
-    public void OnPointerClick(PointerEventData eventData) {
+public class Button : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private LoadSprites loadPanel;
+    private int animIndex = Animator.StringToHash("anim");
+    private bool on;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
         Debug.Log("Click");
+
+        StartGame();
+    }
+
+    private void StartGame()
+    {
         on = true;
-    }
 
-    public void OnPointerDown(PointerEventData eventData) {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnPointerEnter(PointerEventData eventData) {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnPointerExit(PointerEventData eventData) {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnPointerUp(PointerEventData eventData) {
-        throw new System.NotImplementedException();
-    }
-
-    void Update() {
-        if (on) {
-            animator = GetComponent<Animator>();
-            animator.SetBool("anim", true);
-            GameObject.Find("Panel").GetComponent<LoadSprites>().load();
+        if (on)
+        {
+            animator.SetBool(animIndex, true);
+            loadPanel.load();
         }
     }
 }
